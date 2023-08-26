e17c03abfd8521d8bbba1775a3ce80c1f1b7ab95
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-08-26 14:48:45 -0400

    i added the questions

---
+++ 02-Challenge/Develop/index.js
@@ -0,0 +1,61 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter the project description:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license:',
+    choices: ['MIT', 'Apache', 'GNU GPL', 'ISC'], 
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
